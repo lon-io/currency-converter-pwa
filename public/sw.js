95e0e29bb28a13f897102a9cde00005552650c40
Lon Ilesanmi <lonilesanmi@gmail.com>
2018-06-25 18:43:08 +0100

    Add Sass

---
+++ public/sw.js
@@ -0,0 +1,108 @@
+var staticCacheName = 'wittr-static-v8';
+var contentImgsCache = 'wittr-content-imgs';
+var allCaches = [
+  staticCacheName,
+  contentImgsCache
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/skeleton',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('wittr-') &&
+                 !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+    if (requestUrl.pathname.startsWith('/photos/')) {
+      event.respondWith(servePhoto(event.request));
+      return;
+    }
+    // TODO: respond to avatar urls by responding with
+    // the return value of serveAvatar(event.request)
+    if (requestUrl.pathname.startsWith('/avatars/')) {
+      event.respondWith(serveAvatar(event.request));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+function serveAvatar(request) {
+  // Avatar urls look like:
+  // avatars/sam-2x.jpg
+  // But storageUrl has the -2x.jpg bit missing.
+  // Use this url to store & match the image in the cache.
+  // This means you only store one copy of each avatar.
+  var storageUrl = request.url.replace(/-\dx\.jpg$/, '');
+
+  // TODO: return images from the "wittr-content-imgs" cache
+  // if they're in there. But afterwards, go to the network
+  // to update the entry in the cache.
+  return caches.open(contentImgsCache).then(cache => {
+    return cache.match(storageUrl).then(response => {
+      var networkFetch = fetch(request).then((networkResponse) => {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+
+      return response || networkFetch;
+    });
+  });
+  // Note that this is slightly different to servePhoto!
+}
+
+function servePhoto(request) {
+  var storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
+
+  return caches.open(contentImgsCache).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './main.js',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css.[hash]',
+      chunkFilename: '[id].[hash]',
+    })
   ],
+  context: path.join(__dirname, 'src'),
+  entry: {
+    'main.js': './main.js',
+    'styles.css': './assets/styles.js'
+  },
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'main.js',
+    filename: '[name].[hash]',
+    chunkFilename: '[id].[hash]',
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
@@ -18,6 +28,14 @@ module.exports = {
           'babel-loader',
         ],
       },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }
     ],
   },
 };
