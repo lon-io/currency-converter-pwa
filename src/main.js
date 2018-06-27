5ab7f31c0c31d853097ef0e0a059d94b4a9b8aa4
Lon Ilesanmi <lonilesanmi@gmail.com>
2018-06-27 01:07:57 +0100

    Add app-loader

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import './assets/scss/styles.scss';
+import { compile, } from 'handlebars';
+import template from './views/partials/loader.hbs';
 
-console.log('Hello World!');
+window.onload = () => {
+    console.log('Hello World!');
+    const renderedData = compile(template)({});
+
+    const app = document.getElementById('app');
+    app.innerHTML = renderedData;
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,89 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Visualizer = require('webpack-visualizer-plugin');
-const devMode = process.env.NODE_ENV !== 'production'
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   // Include source maps in development files
-  devtool: devMode ?  '#cheap-module-source-map' : false,
+    devtool: devMode ?  '#cheap-module-source-map' : false,
 
-  context: path.join(__dirname, 'src'),
+    context: path.join(__dirname, 'src'),
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'main.[hash].css',
-      chunkFilename: '[id].[hash]',
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'main.[hash].css',
+        chunkFilename: '[id].[hash]',
     }),
-    new CopyWebpackPlugin([{
-      from: './assets/fonts',
-      to: 'fonts',
+      new CopyWebpackPlugin([{
+        from: './assets/fonts',
+        to: 'fonts',
     }, ]),
-    new HtmlWebpackPlugin({
-      title: 'PWA Example',
-      template: path.resolve(__dirname, 'src/views/index.ejs'),
+      new HtmlWebpackPlugin({
+        title: 'PWA Example',
+        template: path.resolve(__dirname, 'src/views/index.ejs'),
     }),
-    new Visualizer(),
+      new Visualizer(),
   ],
 
-  entry: {
-    'main': './main.js',
+    entry: {
+      'main': './main.js',
   },
 
-  resolve: {
-    extensions: ['*', '.js'],
-    modules: [
+    resolve: {
+      extensions: ['*', '.js',],
+      modules: [
         path.resolve(__dirname, 'node_modules'),
     ],
-    alias: {
+      alias: {
         handlebars: 'handlebars/dist/handlebars.min.js',
-    }
+    },
   },
 
-  output: {
-    filename: '[name].[hash].js',
-    chunkFilename: '[id].[hash]',
-    path: path.resolve(__dirname, 'dist'),
+    output: {
+      filename: '[name].[hash].js',
+      chunkFilename: '[id].[hash]',
+      path: path.resolve(__dirname, 'dist'),
   },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
+    module: {
+      rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+            'babel-loader',
         ],
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
+        },
+        {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
             limit: 1000,
-            name: 'fonts/[name].[hash:7].[ext]'
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {
+            name: 'fonts/[name].[hash:7].[ext]',
+        },
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            loader: 'url-loader',
+            query: {
             limit: 10000,
-            name: 'images/[name].[ext].[hash:7]'
-        }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
+            name: 'images/[name].[ext].[hash:7]',
+        },
+        },
+        {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
         ],
-      },
-      {
-        test: /\.handlebars$/,
-        loader: 'text-loader',
-      },
+        },
+        {
+            test: /\.hbs$/,
+            loader: 'text-loader',
+        },
     ],
   },
 };
