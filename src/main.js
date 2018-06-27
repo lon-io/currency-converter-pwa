c0b704d889433a9b81db15da245b2f3b5a8c28e4
Lon Ilesanmi <lonilesanmi@gmail.com>
2018-06-27 01:25:07 +0100

    Add babel support for ES2015; Add Axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
+require('babel-core/register');
+require('babel-polyfill');
+
 import './assets/scss/styles.scss';
 import { compile, } from 'handlebars';
 import template from './views/partials/loader.hbs';
+import axios from 'axios';
+
+const baseUrl = 'https://free.currencyconverterapi.com/api/v5';
+
+const getCurrencies = async () => {
+    try {
+        const response = await axios.get(`${baseUrl}/currencies`);
+        console.log(response.data);
+    } catch(error) {
+        console.log(error);
+    }
+};
 
 window.onload = () => {
     console.log('Hello World!');
+    getCurrencies();
     const renderedData = compile(template)({});
 
     const app = document.getElementById('app');
--- webpack.config.js
+++ webpack.config.js
@@ -12,78 +12,79 @@ module.exports = {
     context: path.join(__dirname, 'src'),
 
     plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'main.[hash].css',
-        chunkFilename: '[id].[hash]',
-    }),
-      new CopyWebpackPlugin([{
-        from: './assets/fonts',
-        to: 'fonts',
-    }, ]),
-      new HtmlWebpackPlugin({
-        title: 'PWA Example',
-        template: path.resolve(__dirname, 'src/views/index.ejs'),
-    }),
-      new Visualizer(),
-  ],
+        new MiniCssExtractPlugin({
+          filename: 'main.[hash].css',
+          chunkFilename: '[id].[hash]',
+      }),
+        new CopyWebpackPlugin([{
+          from: './assets/fonts',
+          to: 'fonts',
+      }, ]),
+        new HtmlWebpackPlugin({
+          title: 'PWA Example',
+          template: path.resolve(__dirname, 'src/views/index.ejs'),
+      }),
+        new Visualizer(),
+    ],
 
-    entry: {
-      'main': './main.js',
-  },
+    entry: [
+        'babel-polyfill',
+        './main.js',
+    ],
 
     resolve: {
-      extensions: ['*', '.js',],
-      modules: [
-        path.resolve(__dirname, 'node_modules'),
-    ],
-      alias: {
-        handlebars: 'handlebars/dist/handlebars.min.js',
+        extensions: ['*', '.js',],
+        modules: [
+          path.resolve(__dirname, 'node_modules'),
+      ],
+        alias: {
+          handlebars: 'handlebars/dist/handlebars.min.js',
+      },
     },
-  },
 
     output: {
-      filename: '[name].[hash].js',
-      chunkFilename: '[id].[hash]',
-      path: path.resolve(__dirname, 'dist'),
-  },
+        filename: '[name].[hash].js',
+        chunkFilename: '[id].[hash]',
+        path: path.resolve(__dirname, 'dist'),
+    },
 
     module: {
-      rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
-            'babel-loader',
-        ],
-        },
-        {
-            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-            loader: 'url-loader',
-            options: {
-            limit: 1000,
-            name: 'fonts/[name].[hash:7].[ext]',
-        },
-        },
-        {
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            loader: 'url-loader',
-            query: {
-            limit: 10000,
-            name: 'images/[name].[ext].[hash:7]',
-        },
-        },
-        {
-            test: /\.(sa|sc|c)ss$/,
-            use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader',
-        ],
-        },
-        {
-            test: /\.hbs$/,
-            loader: 'text-loader',
-        },
-    ],
-  },
+        rules: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: [
+                'babel-loader',
+            ],
+          },
+          {
+              test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+              loader: 'url-loader',
+              options: {
+                limit: 1000,
+                name: 'fonts/[name].[hash:7].[ext]',
+            },
+          },
+          {
+              test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+              loader: 'url-loader',
+              query: {
+                limit: 10000,
+                name: 'images/[name].[ext].[hash:7]',
+            },
+          },
+          {
+              test: /\.(sa|sc|c)ss$/,
+              use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader',
+            ],
+          },
+          {
+              test: /\.hbs$/,
+              loader: 'text-loader',
+          },
+      ],
+    },
 };
